9b5917f1155392c0f963c1175a0cf0e1f6cd16a0
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-22 11:47:05 -0500

    set up mongoose connection. Set up Express to serve static React files. Created postinstall script for Heroku to build static React app.

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+const mongoose = require('mongoose')
 
 var app = express();
+
+// DB setup
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+
+const connection = mongoose.connection
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully')
+})
+
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err)	
+}) 
+
+//serve static react files
+app.use(express.static(__dirname + '/client/build/'))
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +41,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
