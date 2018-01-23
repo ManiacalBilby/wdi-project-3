f929b12b527a3b48ce2c178506330db7202699ee
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-23 17:42:30 -0500

    Set up react Route for HomePage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import styled from 'styled-components'
-import UsersPage from './components/UsersPage'
+import HomePage from './components/HomePage'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
-          <UsersPage/>
+          <Route exact path="/" component={HomePage}/>
       </div>
+      </Router>
     );
   }
 }
