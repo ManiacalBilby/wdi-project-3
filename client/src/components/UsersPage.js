4dc5bf7425563e9bff89c93b1562c0438eb9c801
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-23 16:36:11 -0500

    created components folder in client/src. Added UsersPage component to this folder and set up basic page for testing on local

---
+++ client/src/components/UsersPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class UsersPage extends Component {
+    render() {
+        return (
+            <h1>Users Page!!!</h1>
+        )
+    }
+}
+
+export default UsersPage
