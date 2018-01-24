056786768f5bd8ddc0316185cf9fddfba129a6a6
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-23 22:19:12 -0500

    added proxy to client package.json. Able to make API call and  display data in HomePage component

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
 
 class HomePage extends Component {
+    state = {
+        users: []
+    }
+
+    getAllUsers = () => {
+        axios.get('/api/users')
+            .then(res => {
+                console.log(res.data)
+                this.setState({ users: res.data })
+            })
+    }
+
+    componentWillMount() {
+        this.getAllUsers()
+    }
+
     render() {
+        console.log(this.state.users)
         return (
-            <h1>Home Page!!!</h1>
+            <div>
+                <h1>Home Page!!!</h1>
+                <div>
+                    {this.state.users.map((user) => {
+                        return (<div key={user._id}>
+                            <div>
+                                {user.username}
+                            </div>
+                            <img src={user.photoUrl} alt="user"/>
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
         )
     }
 }
