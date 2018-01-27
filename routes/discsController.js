49b5fea121a0312b6d0477e4652b63f06d986480
ManiacalBilby <tylerwaynemcdonald@gmail.com>
2018-01-27 16:17:25 -0500

    Created and built out discsController in routes

---
+++ routes/discsController.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router({mergeParams: true})
+const User = require('../db/models/User')
+
+router.get('/', (req, res) => {
+    const userId = req.params.userId
+
+    User.findById(userId)
+    .then((user) => {
+        console.log(user.discs)
+        res.json(user.discs)
+    })
+    .catch((error) => console.log(error))
+})
+
+
+
+
+module.exports = router
